aa9a09c7c52f02b0cb864d6cb54b377ad15c6d12
GuillermoOM <guille8ameou@gmail.com>
2024-05-06 00:38:37 -0700

    fixes for Ares, maxim, prometheusS, trebuchet, tornado

--- source/sound/sfx/etg/weapon/largemissileblast.lua
+++ source/sound/sfx/etg/weapon/largemissileblast.lua
@@ -1,47 +1,53 @@
 -- LuaDC version 0.9.19
--- 5/23/2004 7:30:44 PM
+-- 5/23/2004 7:29:12 PM
 -- LuaDC by Age2uN
 -- on error send source file (compiled lua) and this outputfile to Age2uN@gmx.net
 --
 version = 4
-volume = 0.8
-maxPolyphony = 3
-envelope = 
-    { 
-    { 
-        distance = 0, 
-        volume = 0.8, 
-        reverb = 0, 
-        duration = 0, 
-        equalizer = 
-            { 1, 1, 1, 1, 1, 1, 1, 1, }, }, 
-    { 
-        distance = 716.666687, 
-        volume = 0.7, 
-        reverb = 0, 
-        duration = 0, 
-        equalizer = 
-            { 1, 1, 1, 1, 0.75, 0.59, 0.62, 0.62, }, }, 
-    { 
-        distance = 1300, 
-        volume = 0.6, 
-        reverb = 0, 
-        duration = 0, 
-        equalizer = 
-            { 1, 1, 1, 1, 0.75, 0.59, 0.62, 0.62, }, }, 
-    { 
-        distance = 2366.666748, 
-        volume = 0.4, 
-        reverb = 0, 
-        duration = 0, 
-        equalizer = 
-            { 1, 1, 1, 1, 0.6, 0.39, 0.4, 0.32, }, }, 
-    { 
-        distance = 3516.666992, 
-        volume = 0.2, 
-        reverb = 0, 
-        duration = 0, 
-        equalizer = 
-            { 1, 1, 1, 0, 0, 0, 0, 0, }, }, 
-    }
-randSampContainer = 1
+volume = 1
+volumeRand = 0.2
+maxPolyphony = 4
+envelope =
+{
+    {
+        distance = 0,
+        volume = 0.7,
+        reverb = 0,
+        duration = 0,
+        equalizer =
+        { 1, 1, 1, 1, 1, 1, 1, 1, },
+    },
+    {
+        distance = 700,
+        volume = 0.6,
+        reverb = 0,
+        duration = 0,
+        equalizer =
+        { 1, 1, 1, 1, 1, 0.89, 0.8, 0.61, },
+    },
+    {
+        distance = 1500,
+        volume = 0.4,
+        reverb = 0,
+        duration = 0,
+        equalizer =
+        { 1, 1, 1, 1, 0.7, 0.67, 0.69, 0.56, },
+    },
+    {
+        distance = 2500,
+        volume = 0.2,
+        reverb = 0,
+        duration = 0,
+        equalizer =
+        { 1, 1, 1, 1, 0.71, 0.56, 0.46, 0.29, },
+    },
+    {
+        distance = 3500,
+        volume = 0,
+        reverb = 0,
+        duration = 0,
+        equalizer =
+        { 1, 1, 1, 1, 0.4, 0, 0, 0, },
+    },
+}
+randSampContainer = 0
--- source/sound/sfx/ship/gtf_old.lua
+++ source/sound/sfx/ship/gtf_old.lua
@@ -1,4 +1,7 @@
 version = 4
+volume = 1
+volumeRand = 0.2
+maxPolyphony = 4
 envelope =
 {
     {
