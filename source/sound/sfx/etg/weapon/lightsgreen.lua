d9d2f6e639dc63514d0caf75ad3cdd7f72f2f5e4
GuillermoOM <guille8ameou@gmail.com>
2024-05-12 22:30:53 -0700

    fixes on support ships, sentries and beams

--- source/sound/sfx/etg/weapon/lightsgreen.lua
+++ source/sound/sfx/etg/weapon/lightsgreen.lua
@@ -1,89 +1,48 @@
--- This file is generated automatically by the Relic Audio Tool
-
+-- LuaDC version 0.9.19
+-- 5/23/2004 7:29:12 PM
+-- LuaDC by Age2uN
+-- on error send source file (compiled lua) and this outputfile to Age2uN@gmx.net
+--
 version = 4
-
-volume = 0.800000
-envelope = {
-  {
-    distance = 0.000000,
-    volume = 0.370000,
-    reverb = 0.000000,
-    duration = 0,
-    equalizer = {
-              1.000000,
-              1.000000,
-              1.000000,
-              1.000000,
-              1.000000,
-              1.000000,
-              1.000000,
-              1.000000,
+volume = 0.6
+volumeRand = 1
+maxPolyphony = 2
+envelope = 
+    { 
+    { 
+        distance = 0, 
+        volume = 0.6, 
+        reverb = 0, 
+        duration = 0, 
+        equalizer = 
+            { 1, 1, 1, 1, 1, 1, 1, 1, }, }, 
+    { 
+        distance = 1200, 
+        volume = 0.5, 
+        reverb = 0, 
+        duration = 0, 
+        equalizer = 
+            { 1, 1, 1, 1, 0.97, 1, 1, 1, }, }, 
+    { 
+        distance = 2200, 
+        volume = 0.4, 
+        reverb = 0, 
+        duration = 0, 
+        equalizer = 
+            { 1, 1, 1, 1, 0.84, 0.87, 0.77, 0.81, }, }, 
+    { 
+        distance = 3400, 
+        volume = 0.3, 
+        reverb = 0, 
+        duration = 0, 
+        equalizer = 
+            { 1, 1, 1, 0.64, 0.42, 0.31, 0.16, 0, }, }, 
+    { 
+        distance = 5000, 
+        volume = 0.2, 
+        reverb = 0, 
+        duration = 0, 
+        equalizer = 
+            { 1, 1, 1, 0, 0, 0, 0, 0, }, }, 
     }
-  },
-  {
-    distance = 566.666687,
-    volume = 0.360000,
-    reverb = 0.000000,
-    duration = 0,
-    equalizer = {
-              1.000000,
-              1.000000,
-              1.000000,
-              1.000000,
-              1.000000,
-              1.000000,
-              1.000000,
-              1.000000,
-    }
-  },
-  {
-    distance = 1316.666626,
-    volume = 0.290000,
-    reverb = 0.000000,
-    duration = 0,
-    equalizer = {
-              1.000000,
-              1.000000,
-              1.000000,
-              1.000000,
-              1.000000,
-              0.710000,
-              0.720000,
-              0.520000,
-    }
-  },
-  {
-    distance = 2166.666748,
-    volume = 0.200000,
-    reverb = 0.000000,
-    duration = 0,
-    equalizer = {
-              1.000000,
-              1.000000,
-              1.000000,
-              1.000000,
-              0.550000,
-              0.650000,
-              0.560000,
-              0.550000,
-    }
-  },
-  {
-    distance = 3450.000000,
-    volume = 0.000000,
-    reverb = 0.000000,
-    duration = 0,
-    equalizer = {
-              1.000000,
-              1.000000,
-              1.000000,
-              1.000000,
-              0.540000,
-              0.570000,
-              0.520000,
-              0.550000,
-    }
-  },
-}
 randSampContainer = 0
-
--- source/sound/sfx/etg/weapon/slash.lua
+++ source/sound/sfx/etg/weapon/slash.lua
@@ -1,91 +1,48 @@
--- This file is generated automatically by the Relic Audio Tool
-
+-- LuaDC version 0.9.19
+-- 5/23/2004 7:29:12 PM
+-- LuaDC by Age2uN
+-- on error send source file (compiled lua) and this outputfile to Age2uN@gmx.net
+--
 version = 4
-
-volume = 0.900000
-maxPolyphony = 3
-loopCount = 1
-envelope = {
-  {
-    distance = 0.000000,
-    volume = 0.370000,
-    reverb = 0.000000,
-    duration = 0,
-    equalizer = {
-              1.000000,
-              1.000000,
-              1.000000,
-              1.000000,
-              1.000000,
-              1.000000,
-              1.000000,
-              1.000000,
+volume = 0.6
+volumeRand = 1
+maxPolyphony = 2
+envelope = 
+    { 
+    { 
+        distance = 0, 
+        volume = 0.6, 
+        reverb = 0, 
+        duration = 0, 
+        equalizer = 
+            { 1, 1, 1, 1, 1, 1, 1, 1, }, }, 
+    { 
+        distance = 1200, 
+        volume = 0.5, 
+        reverb = 0, 
+        duration = 0, 
+        equalizer = 
+            { 1, 1, 1, 1, 0.97, 1, 1, 1, }, }, 
+    { 
+        distance = 2200, 
+        volume = 0.4, 
+        reverb = 0, 
+        duration = 0, 
+        equalizer = 
+            { 1, 1, 1, 1, 0.84, 0.87, 0.77, 0.81, }, }, 
+    { 
+        distance = 3400, 
+        volume = 0.3, 
+        reverb = 0, 
+        duration = 0, 
+        equalizer = 
+            { 1, 1, 1, 0.64, 0.42, 0.31, 0.16, 0, }, }, 
+    { 
+        distance = 5000, 
+        volume = 0.2, 
+        reverb = 0, 
+        duration = 0, 
+        equalizer = 
+            { 1, 1, 1, 0, 0, 0, 0, 0, }, }, 
     }
-  },
-  {
-    distance = 583.333313,
-    volume = 0.305000,
-    reverb = 0.000000,
-    duration = 0,
-    equalizer = {
-              1.000000,
-              1.000000,
-              1.000000,
-              1.000000,
-              1.000000,
-              1.000000,
-              1.000000,
-              1.000000,
-    }
-  },
-  {
-    distance = 1350.000000,
-    volume = 0.220000,
-    reverb = 0.000000,
-    duration = 0,
-    equalizer = {
-              1.000000,
-              1.000000,
-              1.000000,
-              1.000000,
-              1.000000,
-              1.000000,
-              1.000000,
-              1.000000,
-    }
-  },
-  {
-    distance = 2866.666748,
-    volume = 0.180000,
-    reverb = 0.000000,
-    duration = 0,
-    equalizer = {
-              1.000000,
-              1.000000,
-              1.000000,
-              1.000000,
-              0.670000,
-              0.660000,
-              0.690000,
-              0.570000,
-    }
-  },
-  {
-    distance = 5133.333496,
-    volume = 0.000000,
-    reverb = 0.000000,
-    duration = 0,
-    equalizer = {
-              1.000000,
-              1.000000,
-              1.000000,
-              1.000000,
-              0.000000,
-              0.000000,
-              0.010000,
-              0.000000,
-    }
-  },
-}
 randSampContainer = 0
-
