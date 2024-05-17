5de4e29c76f65bf0c6621c70b2a1e3277409d7ef
GuillermoOM <guille8ameou@gmail.com>
2024-05-16 21:00:23 -0700

    added observer faction

---
+++ source/scripts/races/observer/props/default.lua
@@ -0,0 +1,23 @@
+-- Observer
+
+Number_Properties_Priority = 1.0
+
+Number_Properties = {
+	cfg_race_is_playable = 1.0,
+	cfg_race_index_sort = 1000.0,
+	cfg_race_select_weight = 10.0,
+	cfg_race_is_random = 0,
+	
+	cfg_hyperspace_effect_time = 9.5,
+	cfg_buildable_subsystems = 0.0,
+}
+
+String_Properties_Priority = 1.0
+
+String_Properties = {
+	cfg_race_random_tags_pass = "never_random",
+	cfg_race_random_tags_fail = "",
+
+	cfg_hyperspace_effect_fx = "hyperspace_gate",
+	cfg_hyperspace_effect_audio = "etg/special/SPECIAL_ABILITIES_HYPERSPACE_IN",
+}
