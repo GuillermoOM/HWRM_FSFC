a22049d823178666c499ff4ae9cd73a26cbdbb27
GuillermoOM <guille8ameou@gmail.com>
2024-06-22 15:32:05 -0700

    added thoth

--- source/scripts/races/vasudan/scripts/def_build.lua
+++ source/scripts/races/vasudan/scripts/def_build.lua
@@ -43,6 +43,16 @@ build =
 		"<b>Description:</b> As the slowest fighter in the PVN, the Seth has earned itself a reputation as the Turtle of the Vasudan fleet. Even though it is quite compact in size, the Seth can carry a massive payload, and has a lot of shielding. Its standard impulse engines aren't very powerful, but the Seth gains an incredible speed boost when using its afterburners. The Seth is primarily used is convoy attacks and capital ship escort duties. \n\n<b>Prerequisites:</b> Fighter Design, Multi-Role Swarm Missiles. \n\n<b>Armaments:</b> 4x GVW-5 Prometheus, 1x PVM-MX50, 2x PVM-4 Hornet.",
 	},
 
+	{
+		Type = Ship,
+		ThingToBuild = "hol_thoth",
+		RequiredResearch = "",
+		RequiredShipSubSystems = "",
+		DisplayPriority = 2,
+		DisplayedName = "PVF Thoth - Adv. Space Superiority <c=12AB06>39s</c>",
+		Description =
+		"<b>Description:</b> The Thoth is the most dangerous ship to be created by the PVN. When the Altairian Yards defected to the Hammer of Light rebels, the production of Thoths by the PVN dropped to nil. What makes the Thoth so versatile is its small size, extremely high maneuverability, and advanced weapon systems. It is extremely difficult to hit, in the hands of a skilled pilot. The design for the Thoth was simultaneous with the design for the Ulysses, and these two ships were designed by some of the same technicians. \n\n<b>Prerequisites:</b> Fighter Design, Anti-Fighter Missiles. \n\n<b>Armaments:</b> 4x PVW-15 Avenger (Rapid Fire), 2x PVM-9 Interceptor.",
+	},
 
 
 	-- Bombers --------------
